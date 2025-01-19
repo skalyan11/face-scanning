0381fbf4527ecd9386c5843fa0c41b69489d8db0
skalyan11 <skalyan@umich.edu>
2025-01-18 19:28:28 -0500

    augmented image loading

--- detection.py
+++ detection.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import os
 import cv2
 import albumentations as alb
+import os
 
 # Ensure GPU memory growth is set correctly
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -74,4 +75,81 @@ cv2.rectangle(augmented['image'], start_point, end_point, (255, 0, 0), 2)
 
 # Display the image with the bounding box
 plt.imshow(cv2.cvtColor(augmented['image'], cv2.COLOR_BGR2RGB))
-plt.show()
+#plt.show()
+
+
+
+
+
+IMAGE_DIR = '/Users/srikumarkalyan/Personal/FacialRecognition/face-scanning/data/images'
+
+# Verify directory exists
+if not os.path.exists(IMAGE_DIR):
+    raise Exception(f"Directory not found: {IMAGE_DIR}")
+
+# List all jpg images in directory
+images = [os.path.join(IMAGE_DIR, f) for f in os.listdir(IMAGE_DIR) if f.endswith('.jpg')]
+
+
+#load the training data
+for partition in ['train','test','val']: 
+    for image in images:
+        img = cv2.imread(os.path.join('data', partition, 'images', image))
+
+        coords = [0,0,0.00001,0.00001]
+        label_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', partition, 'labels', f'{image.split(".")[0]}.json')
+        if os.path.exists(label_path):
+            with open(label_path, 'r') as f:
+                label = json.load(f)
+
+            coords[0] = label['shapes'][0]['points'][0][0]
+            coords[1] = label['shapes'][0]['points'][0][1]
+            coords[2] = label['shapes'][0]['points'][1][0]
+            coords[3] = label['shapes'][0]['points'][1][1]
+            height, width = img.shape[:2]
+            coords = list(np.divide(coords, [width,height,width,height]))
+
+        try: 
+            for x in range(60):
+                augmented = augmentations(image=img, bboxes=[coords], class_labels=['face'])
+                cv2.imwrite(os.path.join('aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'), augmented['image'])
+
+                annotation = {}
+                annotation['image'] = image
+
+                if os.path.exists(label_path):
+                    if len(augmented['bboxes']) == 0: 
+                        annotation['bbox'] = [0,0,0,0]
+                        annotation['class'] = 0 
+                    else: 
+                        annotation['bbox'] = augmented['bboxes'][0]
+                        annotation['class'] = 1
+                else: 
+                    annotation['bbox'] = [0,0,0,0]
+                    annotation['class'] = 0 
+
+
+                with open(os.path.join('aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'), 'w') as f:
+                    json.dump(annotation, f)
+
+        except Exception as e:
+            print(e)
+    
+    
+    
+#load augmented images to tensorflow dataset
+train_images = tf.data.Dataset.list_files('aug_data/train/images/*.jpg', shuffle=False)
+train_images = images.map(load_image)
+train_images = train_images.map(lambda x: tf.image.resize(x, (1080, 1500)))
+train_images = train_images.map(lambda x: x/255.0)
+
+test_images = tf.data.Dataset.list_files('aug_data/test/images/*.jpg', shuffle=False)
+test_images = images.map(load_image)
+test_images = test_images.map(lambda x: tf.image.resize(x, (1080, 1500)))
+test_images = test_images.map(lambda x: x/255.0)
+
+val_images = tf.data.Dataset.list_files('aug_data/val/images/*.jpg', shuffle=False)
+val_images = images.map(load_image)
+val_images = images.map(lambda x: tf.image.resize(x, (1080, 1500)))
+val_images = images.map(lambda x: x/255.0)
+
