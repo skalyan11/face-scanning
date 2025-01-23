7bf25238d874b5f736abf14e716b514f872d56b6
skalyan11 <skalyan@umich.edu>
2025-01-22 20:46:35 -0500

    face tracker class completion

--- program.py
+++ program.py
@@ -99,5 +99,104 @@ for idx in range(4):
     ax[idx].imshow(sample_image)
     ax[idx].axis('off')
 
-plt.show()
+#plt.show()
 
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalMaxPooling2D
+from tensorflow.keras.applications import VGG16
+
+vgg = VGG16(include_top=False)
+
+#vgg.summary()
+
+def build_model():
+    input_layer = Input(shape=(1080, 1500, 3))
+    vgg = VGG16(include_top=False, input_tensor=input_layer)
+    
+    f1 = GlobalMaxPooling2D()(vgg.output)
+    class1 = Dense(2048, activation='relu')(f1)
+    class2 = Dense(1, activation='sigmoid')(class1)
+    
+    f2 = GlobalMaxPooling2D()(vgg.output)
+    regress1 = Dense(2048, activation='relu')(f2)
+    regress2 = Dense(4, activation='sigmoid')(regress1)
+    
+    facetracker = Model(inputs=input_layer, outputs=[class2, regress2])
+    return facetracker
+
+    
+facetracker = build_model()
+#facetracker.summary()
+
+
+x, y = train.as_numpy_iterator().next()
+print(x.shape)
+
+
+classes, coords = facetracker.predict(x)
+
+#print(classes, coords)
+
+#slow down the learning to prevent overfitting
+#print(len(train))
+
+batches_per_epoch = len(train)
+lr_decay = (1.0 / 0.75 - 1.0) / batches_per_epoch
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.0001, decay = lr_decay)
+
+#create localization loss
+def localization_loss(y_true, y_pred):
+    delta_cord = tf.reduce_sum(tf.square(y_true - y_pred))
+    
+    h_true = y_true[:, 2] - y_true[:, 0]
+    w_true = y_true[:, 3] - y_true[:, 1]
+    
+    h_pred = y_pred[:, 2] - y_pred[:, 0]
+    w_pred = y_pred[:, 3] - y_pred[:, 1]
+    
+    delta_size = tf.reduce_sum(tf.square(h_true - h_pred) + tf.square(w_true - w_pred))
+    
+    return delta_cord + delta_size
+
+classloss = tf.keras.losses.BinaryCrossentropy()
+regressloss = localization_loss
+
+print(localization_loss(y[1], coords))
+print(classloss(y[0], classes))
+
+
+class FaceTracker(Model):
+    def __init__(self, eyetracker, **kwargs):
+        super().__init__(**kwargs)
+        self.model = eyetracker
+    
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super(FaceTracker, self).compile(**kwargs)
+        self.opt = opt
+        self.closs = classloss
+        self.lloss = localizationloss
+    
+    def train_step(self, batch, **kwargs):
+        x, y = batch
+        
+        with tf.GradientTape() as tape:
+            classes, coords = self.model(x, training=True)
+            batch_classloss = self.closs(y[0], classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+            total_loss = batch_localizationloss + 0.5 * batch_classloss
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+        opt.apply_gradients(zip(grad, self.model.trainable_variables))
+        return {'total_loss': total_loss, 'class_loss': batch_classloss, 'regress_loss': batch_localizationloss}
+    
+    def test_step(self, batch, **kwargs):
+        x, y = batch
+        classes, coords = self.model(x, training=False)
+        batch_classloss = self.closs(y[0], classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss + 0.5 * batch_classloss
+        return {'total_loss': total_loss, 'class_loss': batch_classloss, 'regress_loss': batch_localizationloss}
+    
+        
+    def call(self, x, **kwargs):
+        return self.model(x, **kwargs)
